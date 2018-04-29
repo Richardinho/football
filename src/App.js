d032b39eb72ed458813161ddf1e7bce21225b354
Richard Hunter <richard@richardhunter.co.uk>
2018-04-29 17:18:40 +0100

    styles for links

--- src/App.js
+++ src/App.js
@@ -8,16 +8,54 @@ class App extends Component {
     return (
       <div>
         <div className={styles.App}>
-          <h1>Football</h1>
-          <Link type="light" text="hello world"/>
-          <div className={styles.buttonContainer}>
-            <Button text="press here"/>
-          </div>
-          <div style={{ fontSize: '40px'}}>
-            <Button text="press here"/>
-          </div>
+          <h6>header 6</h6>
+          <h5>header 5</h5>
+          <h4>header 4</h4>
+          <h3>header 3</h3>
+          <h2>header 2</h2>
+          <h1>header 1</h1>
+
+          <ul className={styles.buttonList}>
+            <li style={{ fontSize: '14px' }}>
+              <Link type="light" text="hello world, bling"/>
+            </li>
+            <li style={{ fontSize: '16px' }}>
+              <Link type="light" text="hello world, bling"/>
+            </li>
+            <li style={{ fontSize: '18px' }}>
+              <Link type="light" text="hello world, bling"/>
+            </li>
+            <li style={{ fontSize: '20px' }}>
+              <Link type="light" text="hello world, bling"/>
+            </li>
+            <li style={{ fontSize: '22px' }}>
+              <Link type="light" text="hello world, bling"/>
+            </li>
+            <li style={{ fontSize: '24px' }}>
+              <Link type="light" text="hello world, bling"/>
+            </li>
+          </ul>
+          <ul className={styles.buttonsList}>
+            <li style={{ fontSize: '10px' }}>
+              <Button text="press here"/>
+            </li>
+            <li style={{ fontSize: '14px' }}>
+              <Button text="press here"/>
+            </li>
+            <li style={{ fontSize: '16px' }}>
+              <Button text="press here"/>
+            </li>
+            <li style={{ fontSize: '18px' }}>
+              <Button text="press here"/>
+            </li>
+            <li style={{ fontSize: '20px' }}>
+              <Button text="press here"/>
+            </li>
+            <li style={{ fontSize: '22px' }}>
+              <Button text="press here"/>
+            </li>
+          </ul>
         </div>
-        <Link type="dark" text="this is a dark link"/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'reset-css';
+import './styles/headers.css';
 import './index.module.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
