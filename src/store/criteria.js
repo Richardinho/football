4f692d2774468c9fb1d028e5ba09c9896dc0121c
Richard Hunter <richard@richardhunter.co.uk>
2018-05-01 07:35:02 +0100

    more

--- src/store/criteria.js
+++ src/store/criteria.js
@@ -12,13 +12,13 @@ export const updateCriteriaEpic = (action$, store) =>
   );
       
 
-export const criteriaReducer =  (state = [], action) => {
+export const criteriaReducer = (state = [], action) => {
   switch(action.type) {
     case FULFILLED:
       return action.response.criteria;
     case 'FILTER_ITEM_CHANGED':
       return state.map((filter, index) => {
-        if(index === action.filter) {
+        if (index === action.filter) {
           return { ...filter, items: filter.items.map(item => {
             if (item.key === action.item.key) {
               return { ...item, selected: action.item.selected };
